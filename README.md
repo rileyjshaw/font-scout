42a036e2a15600a7e985df608e6af324df9cf795
Riley Shaw <rileyjshaw@gmail.com>
2021-01-26 23:23:08 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,20 @@
-# Getting Started with Create React App
+# Font comparison tool
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+I made this tool to compare a bunch of local fonts side-by-side. This wasn’t built for public consumption, so it’s just got a bunch of font definitions from [Atipo Foundry](https://www.atipofoundry.com/) hardcoded in. Remix it however you want.
 
-## Available Scripts
+I didn’t include the fonts in the repo. If you [buy them from Atipo](https://www.atipofoundry.com/), copy them to `./public/fonts`.
 
-In the project directory, you can run:
+## Get started
 
-### `yarn start`
+```
+git clone git@github.com:rileyjshaw/font-comparison-tool
+cd font-comparison-tool
+yarn install
+# Copy the Atipo fonts into ./public/fonts,
+# or add some of your own local fonts into the `defaultFonts` array in `App.js`
+yarn start
+```
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Where is the rest of the documentation?
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Sorry, I haven’t written any! [Here’s a quick screen recording instead.](https://youtu.be/TRGodGOu3Ww)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,640 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import Select from "react-select";
+import useHover from "@react-hook/hover";
+import useKeypress from "react-use-keypress";
+
+import "./App.css";
+
+const weights = Object.entries({
+  THIN: 100,
+  EXTRA_LIGHT: 200,
+  LIGHT: 300,
+  REGULAR: 400,
+  BOOK: 450,
+  MEDIUM: 500,
+  SEMI_BOLD: 600,
+  BOLD: 700,
+  ULTRA_BOLD: 800,
+  BLACK: 900,
+  ULTRA_BLACK: 950,
+}).reduce(
+  (acc, [curKey, curVal]) => ({
+    ...acc,
+    [curKey]: {
+      value: curVal,
+      name:
+        curKey === "REGULAR"
+          ? ""
+          : `${curKey[0]}${curKey.slice(1).replace("_", " ")}`.toLowerCase(),
+    },
+  }),
+  {}
+);
+
+const stretchOrder = [
+  "ultra-condensed",
+  "extra-condensed",
+  "condensed",
+  "semi-condensed",
+  "normal",
+  "semi-expanded",
+  "expanded",
+  "extra-expanded",
+  "ultra-expanded",
+];
+
+const defaultFonts = [
+  {
+    name: "Archia",
+    weights: ["REGULAR", "THIN", "LIGHT", "MEDIUM", "SEMI_BOLD", "BOLD"],
+    italics: false,
+  },
+  {
+    name: "Argesta Display",
+    weights: ["REGULAR"],
+    italics: true,
+  },
+  {
+    name: "Argesta Hairline",
+    weights: ["REGULAR"],
+    italics: true,
+  },
+  {
+    name: "Argesta Headline",
+    weights: ["REGULAR"],
+    italics: true,
+  },
+  {
+    name: "Bariol",
+    weights: ["REGULAR", "THIN", "LIGHT", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Bariol Serif",
+    weights: ["REGULAR", "THIN", "LIGHT", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Basier Circle",
+    weights: ["REGULAR", "MEDIUM", "SEMI_BOLD", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Basier Circle Mono",
+    weights: ["REGULAR", "MEDIUM", "SEMI_BOLD", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Basier Square",
+    weights: ["REGULAR", "MEDIUM", "SEMI_BOLD", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Basier Square Mono",
+    weights: ["REGULAR", "MEDIUM", "SEMI_BOLD", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Bould",
+    weights: [
+      "REGULAR",
+      "THIN",
+      "EXTRA_LIGHT",
+      "LIGHT",
+      "MEDIUM",
+      "SEMI_BOLD",
+      "BOLD",
+    ],
+    italics: true,
+  },
+  {
+    name: "Calendas Plus",
+    weights: ["REGULAR", "BOLD"],
+    italics: ["REGULAR"],
+  },
+  {
+    name: "Cassannet Plus",
+    weights: ["REGULAR", "THIN", "LIGHT", "BOLD", "BLACK", "ULTRA_BLACK"],
+    italics: false,
+  },
+  {
+    name: "Chaney",
+    weights: ["REGULAR"],
+    stretches: {
+      REGULAR: {
+        values: ["semi-expanded", "expanded", "ultra-expanded"],
+        italics: false,
+      },
+    },
+    italics: false,
+    aliases: {
+      "semi-expanded": "wide",
+    },
+  },
+  {
+    name: "Geomanist",
+    weights: [
+      "REGULAR",
+      "BOOK",
+      "EXTRA_LIGHT",
+      "THIN",
+      "LIGHT",
+      "MEDIUM",
+      "BOLD",
+      "BLACK",
+      "ULTRA_BLACK",
+    ],
+    italics: true,
+  },
+  {
+    name: "Knile",
+    weights: [
+      "REGULAR",
+      "THIN",
+      "EXTRA_LIGHT",
+      "LIGHT",
+      "MEDIUM",
+      "SEMI_BOLD",
+      "BOLD",
+      "BLACK",
+    ],
+    italics: true,
+  },
+  {
+    name: "MUSETTA",
+    weights: ["REGULAR"],
+    italics: false,
+  },
+  {
+    name: "Noway",
+    weights: ["REGULAR", "THIN", "LIGHT", "MEDIUM", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "Noway Round",
+    weights: ["REGULAR", "THIN", "LIGHT", "MEDIUM", "BOLD"],
+    italics: true,
+  },
+  {
+    name: "PARKING",
+    weights: ["REGULAR"],
+    italics: false,
+  },
+  {
+    name: "Salome",
+    weights: ["REGULAR"],
+    italics: true,
+  },
+  {
+    name: "Salome Deco",
+    weights: ["REGULAR"],
+    italics: false,
+  },
+  {
+    name: "Salome Fine",
+    weights: ["REGULAR"],
+    italics: true,
+  },
+  {
+    name: "Salome Stencil",
+    weights: ["REGULAR"],
+    italics: true,
+  },
+  {
+    name: "Silka",
+    weights: [
+      "REGULAR",
+      "THIN",
+      "EXTRA_LIGHT",
+      "LIGHT",
+      "MEDIUM",
+      "SEMI_BOLD",
+      "BOLD",
+      "BLACK",
+    ],
+    italics: true,
+  },
+  {
+    name: "Silka Mono",
+    weights: [
+      "REGULAR",
+      "THIN",
+      "EXTRA_LIGHT",
+      "LIGHT",
+      "MEDIUM",
+      "SEMI_BOLD",
+      "BOLD",
+      "BLACK",
+    ],
+    italics: true,
+  },
+  {
+    name: "Wotfard",
+    weights: [
+      "REGULAR",
+      "THIN",
+      "EXTRA_LIGHT",
+      "LIGHT",
+      "MEDIUM",
+      "SEMI_BOLD",
+      "BOLD",
+    ],
+    italics: true,
+  },
+].map((font) => ({
+  name: font.name,
+  variants: [
+    ...font.weights.map((weight, i) => ({
+      weight: weights[weight].value,
+      name: weights[weight].name,
+      style: "normal",
+      stretch: "normal",
+    })),
+    ...(typeof font.italics === "boolean"
+      ? font.italics
+        ? font.weights
+        : []
+      : font.italics
+    ).map((weight) => ({
+      weight: weights[weight].value,
+      name: [weights[weight].name, "italic"].filter((x) => x).join(" "),
+      style: "italic",
+      stretch: "normal",
+    })),
+    ...(font.stretches
+      ? Object.entries(font.stretches).flatMap(
+          ([weight, { values, italics }]) =>
+            values.map((stretch) => ({
+              weight: weights[weight].value,
+              name: [weights[weight].name, italics ? "italic" : "", stretch]
+                .filter((x) => x)
+                .join(" "),
+              style: italics ? "italic" : "normal", // TODO: Handle more complex italic / stretch combos if it ever comes up.
+              stretch,
+            }))
+        )
+      : []),
+  ]
+    .map((variant) => ({
+      ...variant,
+      name: [font.name, font.aliases?.[variant.name] ?? variant.name]
+        .filter((x) => x)
+        .join(" "),
+    }))
+    .sort(
+      (a, b) =>
+        a.weight -
+        b.weight +
+        ((a.style === "italic") - (b.style === "italic")) * 10 +
+        (stretchOrder.indexOf(a.stretch) - stretchOrder.indexOf(b.stretch))
+    ),
+  show: true,
+  marked: false,
+  sizeOffset: 0,
+}));
+defaultFonts.forEach(
+  (font) =>
+    (font.activeVariant = font.variants.findIndex(
+      (variant) =>
+        variant.weight === weights.REGULAR.value && variant.style === "normal"
+    ))
+);
+
+function FontContainer({
+  font,
+  onChangeShowToggle,
+  onChangeMarkedToggle,
+  onChangeSizeOffset,
+  onChangeSelect,
+  showSettings,
+  previewContent,
+  alignment,
+  fontSize,
+}) {
+  const ref = useRef(null);
+  const isHovering = useHover(ref);
+
+  const variantOptions = font.variants.map((variant, i) => ({
+    value: i,
+    label: `${variant.name}`,
+  }));
+
+  return (
+    <li ref={ref} className="font-container">
+      {showSettings && (
+        <div className="font-settings">
+          <input
+            className="show-font-toggle"
+            type="checkbox"
+            checked={font.show}
+            onChange={onChangeShowToggle}
+            id={`show-font-toggle-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <label
+            htmlFor={`show-font-toggle-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <input
+            className="marked-font-toggle"
+            type="checkbox"
+            checked={font.marked}
+            onChange={onChangeMarkedToggle}
+            id={`marked-font-toggle-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <label
+            htmlFor={`marked-font-toggle-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <Select
+            value={variantOptions[font.activeVariant]}
+            options={variantOptions}
+            className="select-font-variant"
+            styles={{
+              option: (provided, state) => ({
+                ...provided,
+                fontFamily: font.name,
+                fontWeight: font.variants[state.value].weight,
+                fontStyle: font.variants[state.value].style,
+                fontStretch: font.variants[state.value].stretch,
+              }),
+              singleValue: (provided, state) => ({
+                ...provided,
+                fontFamily: font.name,
+                fontWeight: font.variants[state.data.value].weight,
+                fontStyle: font.variants[state.data.value].style,
+                fontStretch: font.variants[state.data.value].stretch,
+              }),
+            }}
+            onChange={onChangeSelect}
+          />
+          <input
+            className="font-size-offset-input"
+            type="number"
+            value={font.sizeOffset}
+            onChange={onChangeSizeOffset}
+          />
+        </div>
+      )}
+      {!showSettings && isHovering && (
+        <div className="font-hover-settings">
+          <input
+            className="show-font-toggle"
+            type="checkbox"
+            checked={font.show}
+            onChange={onChangeShowToggle}
+            id={`show-font-toggle-hover-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <label
+            htmlFor={`show-font-toggle-hover-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <input
+            className="marked-font-toggle"
+            type="checkbox"
+            checked={font.marked}
+            onChange={onChangeMarkedToggle}
+            id={`marked-font-toggle-hover-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+          <label
+            htmlFor={`marked-font-toggle-hover-${font.name
+              .toLowerCase()
+              .replace(/[ _]/g, "-")}`}
+          />
+        </div>
+      )}
+      <pre
+        className={`font-preview${font.marked ? " marked" : ""}`}
+        style={{
+          textAlign: alignment,
+          fontFamily: font.name,
+          fontWeight: font.variants[font.activeVariant].weight,
+          fontSize: `${fontSize + font.sizeOffset}px`,
+          fontStyle: font.variants[font.activeVariant].style,
+          fontStretch: font.variants[font.activeVariant].stretch,
+        }}
+        title={font.variants[font.activeVariant].name}
+      >
+        {font.show && previewContent}
+      </pre>
+    </li>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [fonts, setFonts] = useState(defaultFonts);
+  const [previewContent, setPreviewContent] = useState(
+    "The thundering machines sputtered…\n\n…and stopped."
+  );
+  const [fontSize, setFontSize] = useState(22);
+  const [fontWeight, setFontWeight] = useState(400);
+  const [alignment, setAlignment] = useState("center");
+  const [configMode, setConfigMode] = useState(true);
+  const [comparisonMode, setComparisonMode] = useState(false);
+  const [comparisonIdx, setComparisonIdx] = useState(0);
+
+  const markedFonts = fonts.filter((font) => font.marked && font.show);
+
+  useKeypress("Escape", () => {
+    setComparisonMode(false);
+    setComparisonIdx(0);
+  });
+  useKeypress("ArrowLeft", () => {
+    if (comparisonMode)
+      setComparisonIdx(
+        (i) => (markedFonts.length + (i - 1)) % markedFonts.length
+      );
+  });
+  useKeypress("ArrowRight", () => {
+    if (comparisonMode)
+      setComparisonIdx(
+        (i) => (markedFonts.length + (i - 1)) % markedFonts.length
+      );
+  });
+
+  if (comparisonMode) {
+    const font = markedFonts[comparisonIdx];
+    return (
+      <div className="comparison-mode">
+        <pre
+          className="font-preview"
+          style={{
+            textAlign: alignment,
+            fontFamily: font.name,
+            fontWeight: font.variants[font.activeVariant].weight,
+            fontSize: `${fontSize + font.sizeOffset}px`,
+            fontStyle: font.variants[font.activeVariant].style,
+            fontStretch: font.variants[font.activeVariant].stretch,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {previewContent}
+        </pre>
+        <div className="comparison-mode-font-name">
+          {font.variants[font.activeVariant].name}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={markedFonts.length ? "comparison-button-visible" : ""}>
+      <div
+        className={`global-settings${
+          configMode ? " show-global-settings" : ""
+        }`}
+      >
+        <label>
+          Font size:
+          <input
+            className="global-font-size-input"
+            type="number"
+            value={fontSize}
+            onChange={(e) => setFontSize(+e.target.value)}
+          />
+        </label>
+        <label>
+          Nearest weight:
+          <input
+            className="global-font-weight-input"
+            type="number"
+            value={fontWeight}
+            step={100}
+            onChange={(e) => {
+              const targetWeight = +e.target.value;
+              setFontWeight(targetWeight);
+              setFonts((fonts) =>
+                fonts.map((font) => {
+                  const activeStyle = font.variants[font.activeVariant].style;
+                  return {
+                    ...font,
+                    activeVariant: font.variants
+                      .map((variant, index) => ({
+                        index,
+                        score:
+                          Math.abs(variant.weight - targetWeight) -
+                          (variant.style === activeStyle),
+                      }))
+                      .sort((a, b) => a.score - b.score)[0].index,
+                  };
+                })
+              );
+            }}
+          />
+        </label>
+        <textarea
+          rows={3}
+          className="preview-text-input"
+          value={previewContent}
+          onChange={(e) => setPreviewContent(e.target.value)}
+        />
+        <fieldset className="alignment-options">
+          Align:&nbsp;
+          <input
+            type="radio"
+            value="left"
+            checked={alignment === "left"}
+            onChange={(e) => {
+              setAlignment(e.target.value);
+            }}
+          />
+          <input
+            type="radio"
+            value="center"
+            checked={alignment === "center"}
+            onChange={(e) => {
+              setAlignment(e.target.value);
+            }}
+          />
+          <input
+            type="radio"
+            value="right"
+            checked={alignment === "right"}
+            onChange={(e) => {
+              setAlignment(e.target.value);
+            }}
+          />
+        </fieldset>
+        <input
+          className="config-mode-toggle"
+          type="checkbox"
+          checked={configMode}
+          onChange={(e) => setConfigMode(e.target.checked)}
+          id="config-mode-toggle"
+        />
+        <label htmlFor="config-mode-toggle"></label>
+      </div>
+
+      <ul className="font-containers">
+        {fonts
+          .sort((a, b) => b.show - a.show || a.name.localeCompare(b.name))
+          .map((font, i) => {
+            if (!configMode && !font.show) return null;
+
+            return (
+              <FontContainer
+                key={font.name}
+                font={font}
+                onChangeShowToggle={(e) =>
+                  setFonts((fonts) => [
+                    ...fonts.slice(0, i),
+                    {
+                      ...fonts[i],
+                      show: e.target.checked,
+                      marked: e.target.checked && fonts[i].marked,
+                    },
+                    ...fonts.slice(i + 1),
+                  ])
+                }
+                onChangeMarkedToggle={(e) =>
+                  setFonts((fonts) => [
+                    ...fonts.slice(0, i),
+                    { ...fonts[i], marked: e.target.checked },
+                    ...fonts.slice(i + 1),
+                  ])
+                }
+                onChangeSizeOffset={(e) =>
+                  setFonts((fonts) => [
+                    ...fonts.slice(0, i),
+                    { ...fonts[i], sizeOffset: +e.target.value },
+                    ...fonts.slice(i + 1),
+                  ])
+                }
+                onChangeSelect={(newActiveVariant) =>
+                  setFonts((fonts) => [
+                    ...fonts.slice(0, i),
+                    {
+                      ...fonts[i],
+                      activeVariant: newActiveVariant.value,
+                    },
+                    ...fonts.slice(i + 1),
+                  ])
+                }
+                showSettings={configMode}
+                previewContent={previewContent}
+                fontSize={fontSize}
+                alignment={alignment}
+              />
+            );
+          })}
+      </ul>
+      {markedFonts.length > 1 && (
+        <button
+          className="start-comparison-button"
+          onClick={() => setComparisonMode(true)}
+        >
+          Compare {markedFonts.length} marked fonts
+        </button>
+      )}
     </div>
   );
 }
