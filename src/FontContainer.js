7c22d24ff2a2011c46edbc818f33b9aac351ac4d
Riley Shaw <rileyjshaw@gmail.com>
2021-02-07 21:38:52 -0800

    Bugfix: extra quote fontFamily names

--- src/FontContainer.js
+++ src/FontContainer.js
@@ -61,14 +61,14 @@ const FontContainer = memo(function FontContainer({
             styles={{
               option: (provided, state) => ({
                 ...provided,
-                fontFamily: font.name,
+                fontFamily: `"${font.name}"`,
                 fontWeight: font.variants[state.value].weight,
                 fontStyle: font.variants[state.value].style,
                 fontStretch: font.variants[state.value].stretch,
               }),
               singleValue: (provided, state) => ({
                 ...provided,
-                fontFamily: font.name,
+                fontFamily: `"${font.name}"`,
                 fontWeight: font.variants[state.data.value].weight,
                 fontStyle: font.variants[state.data.value].style,
                 fontStretch: font.variants[state.data.value].stretch,
@@ -121,7 +121,7 @@ const FontContainer = memo(function FontContainer({
         <pre
           className={`font-preview${font.marked ? " marked" : ""}`}
           style={{
-            fontFamily: font.name,
+            fontFamily: `"${font.name}"`,
             fontWeight: font.variants[font.activeVariant].weight,
             fontSize: `${font.sizeOffset}em`,
             fontStyle: font.variants[font.activeVariant].style,
