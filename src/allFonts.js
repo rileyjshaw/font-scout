f54e648d414205828e7c378e64159f09a5e54e60
Riley Shaw <rileyjshaw@gmail.com>
2023-01-21 13:30:15 -0800

    Add more starred fonts

--- src/allFonts.js
+++ src/allFonts.js
@@ -76,7 +76,7 @@ const allFonts = [...googleFonts, ...localFonts]
 	.sort((a, b) => a.name.localeCompare(b.name));
 
 // TODO: Save these to a DB or something.
-const STARRED_FONTS = ['Lexend', 'Atkinson Hyperlegible', 'Ovo', 'Trispace', 'Epilogue', 'Spectral', 'Manrope'];
+const STARRED_FONTS = ['Lexend', 'Atkinson Hyperlegible', 'Ovo', 'Trispace', 'Epilogue', 'Spectral', 'Manrope', 'Cousine', 'IBM Plex Mono'];
 const UI_FONTS = ['Helvetica', 'Default Sans', 'Output Sans 2 Beta', 'Inter'];
 
 allFonts.forEach(font => {
