4e379e0226f5df80864559034e55c59caac28c0c
Riley Shaw <rileyjshaw@gmail.com>
2025-05-27 15:16:54 -0400

    Style tweaks

--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -6,7 +6,7 @@ const TextArea = React.forwardRef(({ className, ...props }, ref) => {
 	return (
 		<textarea
 			className={cn(
-				'flex rounded-sm grow resize-none border border-input bg-transparent px-2 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+				'flex rounded-sm grow resize-none border border-input bg-background px-2 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
 				className
 			)}
 			ref={ref}
